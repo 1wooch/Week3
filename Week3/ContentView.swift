06d42bbbcce90953146c5117c972b5cd6d62ae0f
wwc <onechoo1106@gmail.com>
2023-03-20 19:20:00 +1000

    make button count

--- Week3/ContentView.swift
+++ Week3/ContentView.swift
@@ -7,13 +7,17 @@
 
 import SwiftUI
 
+var count = 1
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Text("Count = \(count)")
+            Button("add count"){
+                count+=1
+                print(count)
+            }
         }
         .padding()
     }
