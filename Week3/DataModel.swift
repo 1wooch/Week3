86684b4c91e352138bccbe5075a6ad1d3ea0c7bb
wwc <onechoo1106@gmail.com>
2023-03-20 18:00:33 +1000

    Initial Commit

---
+++ Week3/DataModel.swift
@@ -0,0 +1,23 @@
+//
+//  DataModel.swift
+//  Week3
+//
+//  Created by Wonwoo Choi on 20/3/2023.
+//
+
+import Foundation
+
+struct Scientist{
+    var name:String
+    var img:String
+}
+
+var  testData = [
+    Scientist(name: "Newton", img: "newton"),
+    Scientist(name: "Einstein", img: "einstein")
+]
+
+func getScientistName(_ idx:Int)->String{
+    return testData[idx].name
+}
+ 
---
+++ Week3/Week3App.swift
@@ -0,0 +1,17 @@
+//
+//  Week3App.swift
+//  Week3
+//
+//  Created by Wonwoo Choi on 20/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
