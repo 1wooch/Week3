c10a28c26f676dcf75fcd2917092f2343499db36
wwc <onechoo1106@gmail.com>
2023-03-20 19:39:44 +1000

    text box

--- Week3/ContentView.swift
+++ Week3/ContentView.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-var count = 1
 
 
 struct ContentView: View {
+    @State var count = 1
+
     var body: some View {
         VStack {
            Text("Count = \(count)")
@@ -18,13 +19,31 @@ struct ContentView: View {
                 count+=1
                 print(count)
             }
+            CountView(ct:$count)
         }
         .padding()
     }
 }
 
+struct CountView:View{
+    @Binding var ct:Int
+    var body:some View{
+        Text("CountView \(ct)")
+    }
+}
+
+struct TextFieldView:View{
+    @State var name = "WWC"
+    var body: some View{
+        VStack{
+            Text("Name is \(name)")
+            TextField("Input your name", text: $name)
+        }.padding()
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TextFieldView()
     }
 }
