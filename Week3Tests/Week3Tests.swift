2e4d1d7b1a7d55f8b8996271e5cafac04fee2531
wwc <onechoo1106@gmail.com>
2023-03-20 18:30:31 +1000

    test done

--- Week3Tests/Week3Tests.swift
+++ Week3Tests/Week3Tests.swift
@@ -18,17 +18,15 @@ final class Week3Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-    func TestExample() throws{
+    
+    func testExample() throws{
         let p = testData[0]
-        
-        XCTAssertNil(p)
+        let p2 = testData[1]
+        XCTAssertNotNil(p)
+        //XCTAssertNil(p2)//testExample(): XCTAssertNil failed: "Scientist(name: "Einstein", img: "einstein")"
+        XCTAssertNotNil(p2)
+        XCTAssertEqual(p.name,"Newton")
+        XCTAssertEqual(getScientistName(1),"Einstein")
     }
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
